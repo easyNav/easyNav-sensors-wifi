e7f0f06dafe0b312b0de9c326e27ae9798fec308
Joel Tong <me@joeltong.org>
2014-10-12 22:27:06 +0800

    first commit

---
+++ easyNav_sensors_wifi/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# This file is part of easyNav-sensors-wifi.
+# https://github.com/easyNav/easyNav-sensors-wifi
+
+# Licensed under the MIT license:
+# http://www.opensource.org/licenses/MIT-license
+# Copyright (c) 2014 Joel Tong me@joeltong.org
+
+from easyNav_sensors_wifi.version import __version__
